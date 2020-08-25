e3143694ec00f827c1201bd5eb6ac00024a676c5
Martin <mgaraypagano@gmail.com>
2020-08-25 10:08:46 -0300

    creción y diseño del componente navbar, updates en App y About

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import About from "./Components/About/about";
 import Portfolio from "./Components/Portfolio/portfolio";
 import Footer from "./Components/Footer/footer";
+import Navbar from "./Components/Navbar/navbar";
 
 function App() {
   return (
     <div className="App">
       <Footer>
+        <Navbar />
         <About />
         <Portfolio />
       </Footer>
---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+class Navbar extends React.Component {
+  state = {
+    show: true,
+    scrollPosition: 0,
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    this.setState({
+      scrollPosition: document.body.getBoundingClientRect().top,
+      show:
+        document.body.getBoundingClientRect().top > this.state.scrollPosition,
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    return (
+      <nav className={this.state.show ? "active" : "hidden"}>
+        <ul>
+          <li>Inicio</li>
+          <li>Sobre mí</li>
+          <li>Proyectos</li>
+          <li>Habilidades</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
